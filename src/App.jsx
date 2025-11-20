f706c1138846a05570abf4632423b55fc1cdd382
Ayan Khan <beaayank@gmail.com>
2025-11-20 22:27:04 +0530

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar"
 
 const App = () => {
     return (
-        <div>
-            <h1 className="text-3xl text-center my-20 text-blue-300 font-bold underline">Hello, Apple Macbook Pro!</h1>
-        </div>
+        <main>
+            <Navbar />
+        </main>
     )
 }
 
