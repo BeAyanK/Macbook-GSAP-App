06ef35953a5e563d145ebf1be4a33ceca0567edb
Ayan Khan <beaayank@gmail.com>
2025-11-20 21:37:05 +0530

    app setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-center my-20 text-blue-300 font-bold underline">Hello, Apple Macbook Pro!</h1>
+        </div>
+    )
+}
+
+export default App
