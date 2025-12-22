211d98b2a981ce33546816cf9847127e4f59160b
Ayan Khan <beaayank@gmail.com>
2025-12-22 17:33:15 +0530

    Implemented Performance, Features, Highlights, and Footer. Fixed models too. full completed.

--- src/components/three/StudioLights.jsx
+++ src/components/three/StudioLights.jsx
@@ -1,5 +1,4 @@
-import { Environment, Lightformer } from "@react-three/drei"
-
+import {Environment, Lightformer} from "@react-three/drei";
 
 const StudioLights = () => {
     return (
@@ -9,40 +8,38 @@ const StudioLights = () => {
                     <Lightformer
                         form="rect"
                         intensity={10}
-                        position={[-10,5,-5]}
+                        position={[-10, 5, -5]}
                         scale={10}
                         rotation-y={Math.PI / 2}
                     />
                     <Lightformer
                         form="rect"
                         intensity={10}
-                        position={[10,0,1]}
+                        position={[10, 0, 1]}
                         scale={10}
                         rotation-y={Math.PI / 2}
                     />
                 </group>
             </Environment>
-            <spotLight 
-                position={[-2,10,5]}
+            <spotLight
+                position={[-2, 10, 5]}
                 angle={0.15}
                 decay={0}
                 intensity={Math.PI * 0.2}
             />
-            <spotLight 
-                position={[0,-25,10]}
+            <spotLight
+                position={[0, -25, 10]}
                 angle={0.15}
                 decay={0}
                 intensity={Math.PI * 0.2}
             />
-            <spotLight 
-                position={[0,15,5]}
+            <spotLight
+                position={[0, 15, 5]}
                 angle={0.15}
                 decay={0.1}
                 intensity={Math.PI * 1}
             />
-
         </group>
     )
 }
-
 export default StudioLights
